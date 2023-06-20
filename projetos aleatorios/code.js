b33c749cf3ac9412fd3275b94aec82a17c6262d3
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-06-20 10:33:07 -0300

    bait

--- projetos aleatorios/code.js
+++ projetos aleatorios/code.js
@@ -1,8 +1,37 @@
-function No(){
+var m = true;
 
-    var btn = document.getElementById("no");
-    var pos = Math.random() * (500 + 50) + "px";
-    btn.style.top = pos;
-    btn.style.left = pos;
+function miss(){
+
+    if(m){
+        var btn = document.getElementById("no");
+        var top = Math.random() * (1000 + 50) + "px";
+        var left = Math.random() * (500 + 50) + "px";
+        btn.style.top = top;
+        btn.style.left = left;
+        txt.innerHTML = "Dumb";
+    }
 
 }
+
+function yes(){
+
+    var txt = document.getElementById("txt");
+    txt.innerHTML = "Dumb";
+
+}
+
+function no(){
+
+    var txt = document.getElementById("txt");
+    txt.innerHTML = "Smart";
+
+}
+
+function stop(){
+
+    /*let btn = document.getElementById("no");
+    btn.style.position = "static";*/
+    m = false;
+
+}
+//window.history.back();
