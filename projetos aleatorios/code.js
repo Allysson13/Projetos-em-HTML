23465d40929693fab12826ab0951c7cea3ed367b
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-06-20 10:29:27 -0300

    bait

--- projetos aleatorios/code.js
+++ projetos aleatorios/code.js
@@ -1,6 +1,8 @@
 function No(){
 
-    var position = Math.random() * 500 + 50;
-    document.getElementById("no").style.position = position;
+    var btn = document.getElementById("no");
+    var pos = Math.random() * (500 + 50) + "px";
+    btn.style.top = pos;
+    btn.style.left = pos;
 
 }
