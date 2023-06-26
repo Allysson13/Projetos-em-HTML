aa5fa5fd0859ceb5e1b2747a42b98998663fffe5
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-06-26 14:15:02 -0300

    exemplo

--- projetos aleatorios/code.js
+++ projetos aleatorios/code.js
@@ -3,9 +3,9 @@ var m = true;
 function miss(){
 
     if(m){
-        var btn = document.getElementById("no");
-        var top = Math.random() * (1000 + 50) + "px";
-        var left = Math.random() * (500 + 50) + "px";
+        let btn = document.getElementById("no");
+        let top = Math.random() * (1000 + 50) + "px";
+        let left = Math.random() * (500 + 50) + "px";
         btn.style.top = top;
         btn.style.left = left;
         txt.innerHTML = "Burro";
@@ -15,14 +15,14 @@ function miss(){
 
 function yes(){
 
-    var txt = document.getElementById("txt");
+    let txt = document.getElementById("txt");
     txt.innerHTML = "Burro";
 
 }
 
 function no(){
 
-    var txt = document.getElementById("txt");
+    let txt = document.getElementById("txt");
     txt.innerHTML = "Inteligente";
 
 }
@@ -37,10 +37,33 @@ function stop(){
 
 function trocar(){
 
-    var btn1 = document.getElementById("n1");
-    var btn2 = document.getElementById("n2");
-    n1 = ntn1.innerHTML;
-    
+    let txt1 = document.getElementById("v1");
+    let txt2 = document.getElementById("v2");
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    txt1.innerHTML = n2;
+    txt2.innerHTML = n1;
+    console.log("teste");
+
+}
+
+function verificar(){
+
+    var idade = document.getElementById("n1").value;
+    var dependentes = document.getElementById("n2").value;
+    if(idade < 40){
+
+        alert("Aumento de 10% " + idade);
+
+    }else if(idade <= 55 && dependentes > 1){
+
+        alert("Aumento de 25%");
+
+    }else if(dependentes > 3){
+
+        alert("Aumento de 30%");
+
+    }
 
 }
 
