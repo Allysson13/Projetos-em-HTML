fea84281a9076222300c5429b441cc9704033cff
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-06-27 10:53:40 -0300

    calculadora incompleta

--- projetos aleatorios/code.js
+++ projetos aleatorios/code.js
@@ -1,8 +1,8 @@
 var m = true;
 
-function miss(){
+function miss() {
 
-    if(m){
+    if (m) {
         let btn = document.getElementById("no");
         let top = Math.random() * (1000 + 50) + "px";
         let left = Math.random() * (500 + 50) + "px";
@@ -13,21 +13,21 @@ function miss(){
 
 }
 
-function yes(){
+function yes() {
 
     let txt = document.getElementById("txt");
     txt.innerHTML = "Burro";
 
 }
 
-function no(){
+function no() {
 
     let txt = document.getElementById("txt");
     txt.innerHTML = "Inteligente";
 
 }
 
-function stop(){
+function stop() {
 
     /*let btn = document.getElementById("no");
     btn.style.position = "static";*/
@@ -35,7 +35,7 @@ function stop(){
 
 }
 
-function trocar(){
+function trocar() {
 
     let txt1 = document.getElementById("v1");
     let txt2 = document.getElementById("v2");
@@ -47,20 +47,20 @@ function trocar(){
 
 }
 
-function verificar(){
-    
+function verificar() {
+
     let txt3 = document.getElementById("v3");
     var idade = document.getElementById("n1").value;
     var dependentes = document.getElementById("n2").value;
-    if(idade < 40){
+    if (idade < 40) {
 
         txt3.innerHTML += "10%";
 
-    }else if(idade <= 55 || dependentes > 1){
+    } else if (idade <= 55 || dependentes > 1) {
 
         txt3.innerHTML += "25%";
 
-    }else if(dependentes > 3){
+    } else if (dependentes > 3) {
 
         txt3.innerHTML += "30%";
 
@@ -68,13 +68,94 @@ function verificar(){
 
 }
 
-function tabuada(){
+function tabuada(operacao) {
 
     let n = parseInt(document.getElementById("n1").value);
     let txt = document.getElementById("n2");
-    for(let i = 1; i <= 10; i++){
 
-        txt.value += n + " + " + i + " = " + (n + i) + "\n";
+    if(operacao == "+"){
+
+        adição(n, txt);
+
+    }else if(operacao == "-"){
+
+        subtração(n, txt);
+
+    }else if(operacao == "*"){
+
+        multiplicação(n, txt);
+
+    }else if(operacao == "/"){
+
+        divisão(n, txt);
+
+    }else if(operacao == "^"){
+
+        exponencial(n, txt);
+
+    }else{
+
+        raiz(n, txt);
+
+    }
+
+    function adição(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n + i) + "\n";
+
+        }
+
+    }
+
+    function subtração(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n - i) + "\n";
+
+        }
+
+    }
+
+    function multiplicação(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n * i) + "\n";
+
+        }
+
+    }
+
+    function divisão(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n / i) + "\n";
+
+        }
+
+    }
+
+    function exponencial(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n ** i) + "\n";
+
+        }
+
+    }
+
+    function raiz(n, txt) {
+
+        for (let i = 1; i <= 10; i++) {
+
+            txt.value += n + " + " + i + " = " + (n ** 1/i) + "\n";
+
+        }
 
     }
 
